e60f67f0c13794af47be8684398ac7267ef8578d
aljosa <aljosagadjanskibozic@gmail.com>
2023-04-25 23:19:09 +0200

    First Commit

--- src/main/java/rs/singidunum/ac/rs/OnlineStore/OnlineStoreApplication.java
+++ src/main/java/rs/singidunum/ac/rs/OnlineStore/OnlineStoreApplication.java
@@ -2,12 +2,21 @@ package rs.singidunum.ac.rs.OnlineStore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class OnlineStoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineStoreApplication.class, args);
+	}
+
+
+	@GetMapping("/root")
+	public String apiRoot() {
+		return "Hello World";
 	}
 
 }
